e0851586960907459fa3e3f7ead70082883fe853
Your Name <myselfsayak2003@gmail.com>
2023-03-21 20:35:10 +0530

    Routes Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_start/Pages/home_page.dart';
+import 'package:flutter_start/utlis/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'Pages/login_pages.dart';
 
@@ -25,8 +26,8 @@ class MyApp extends StatelessWidget {
       // initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
       },
     );
   }
